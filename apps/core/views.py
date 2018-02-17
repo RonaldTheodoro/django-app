888c684b647eac1ec6c9a5a98c32c1ff8e915947
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-02-17 18:38:04 -0200

    write txt

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.forms import model_to_dict
+import os
 
-from . import models, forms
+from django.conf import settings
+from django.forms import model_to_dict
+from django.shortcuts import get_object_or_404, render
+
+from . import forms, models
+
 
 def index(request):
 
-    if request.method == 'GET': # If the form is submitted
+    if request.method == 'GET':
         search_query = request.GET.get('search_box', None)
 
         if search_query is not None:
@@ -18,6 +22,28 @@ def index(request):
         form = forms.NfeForm(request.POST)
 
         if form.is_valid():
-            print('ok')
+            nfe_txt_creator(form)
 
     return render(request, 'index.html', {'form': form})
+
+
+def nfe_txt_creator(form):
+    email = str(form.cleaned_data.get('email'))
+    file_path = os.path.join(settings.BASE_DIR, f'nfe-{email}.txt')
+    with open(file_path, 'w') as file:
+        write_file(file, form)
+
+
+def write_file(file, form):
+    nfe_txt = 'Nome: {}\nCPF: {}\nE-Mail: {}\nData de nascimento: {}'
+    nfe_txt = nfe_txt.format(
+        form_field(form, 'name'),
+        form_field(form, 'cpf'),
+        form_field(form, 'email'),
+        form_field(form, 'birth_date')
+    )
+    file.write(nfe_txt)
+
+
+def form_field(form, field):
+    return str(form.cleaned_data.get(field))
